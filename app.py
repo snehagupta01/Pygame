77c5da1a9670331ec8346f5fa3f9b4cc76a6cab3
snehagupta01 <sneha2001gupta@gmail.com>
2020-03-25 17:19:13 +0530

    event loop 2

--- app.py
+++ app.py
@@ -1,26 +1,49 @@
 import pygame,sys,os
 from pygame.locals import *
 
-red = [255,0,0]#(255,0,0)
+catx = 10
+caty = 10
+screen = 0
+def myquit():
+    pygame.quit()
+    sys.exit()
 
-#initialize pygame
-pygame.init()
-
-#setup window
-window = pygame.display.set_mode((1000,600))
-pygame.display.set_caption("SNAKE GAME")
-
-#set up drawing surface
-screen = pygame.display.get_surface()
-screen.fill(red)
-pygame.display.set_caption("Snake")
-pygame.display.flip()
-while True:
-    #  print("hi")
-    for event in pygame.event.get():
-        print(event)
+def check_inputs(events):
+    global catx,caty,screen
+    for event in events:
         if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+            quit()
+        else:
+            if event.type == KEYDOWN:
+                if event.key==K_ESCAPE:
+                    myquit()
+                elif event.key==K_LEFT:
+                    catx-=5
+                    print("move rect left")
+                elif event.key==K_RIGHT:
+                    catx+=5
+                    print("move rect right")
+                else:
+                    print(event.key)
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen,(255,255,255),(catx,50,50,10))
+    pygame.display.update()            
+    
+def main():
+    global screen
+    #initialize pygame
+    pygame.init()
+    #setup screen
+    SCREEN_WIDTH = 600
+    SCREEN_HEIGHT = 480
+    #setup window
+    window = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    pygame.display.set_caption("SNAKE GAME")
+    #set up drawing surface
+    screen = pygame.display.get_surface()
+
     pygame.display.update()
 
+    while True:
+        check_inputs(pygame.event.get())
+main()
